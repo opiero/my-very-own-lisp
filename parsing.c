cc027a91af6497d94d3cccd57c1b2c24593e6ab8
Piero Capelo <piero.capelo@gmail.com>
2022-04-18 12:16:15 -0300

    started error handling. bug: division by zero not printing the error

--- parsing.c
+++ parsing.c
@@ -87,6 +87,19 @@ void lval_println(lval v) {
     putchar('\n');
 }
 
+lval eval_op(lval x, char *op, lval y){
+    if (x.type == LVAL_ERR) { return x; }
+    if (y.type == LVAL_ERR) { return y; }
+
+    if (strcmp(op, "+") == 0) {return lval_num (x.num + y.num);}
+    if (strcmp(op, "-") == 0) {return lval_num (x.num - y.num);}
+    if (strcmp(op, "*") == 0) {return lval_num (x.num * y.num);}
+    if (strcmp(op, "/") == 0) {
+        return y.num == 0 ? lval_err(LERR_DIV_ZERO) : lval_num(x.num / y.num);
+    }
+    return lval_err(LERR_BAD_OP);
+}
+
 int number_of_nodes(mpc_ast_t* t) {
     if (t->children_num == 0) return 1;
     if (t->children_num >= 1) {
@@ -100,22 +113,17 @@ int number_of_nodes(mpc_ast_t* t) {
     return 0;
 }
 
-long long eval_op (long long x, char* op, long long y) {
-    if (strcmp(op, "+") == 0) return x+y;
-    if (strcmp(op, "-") == 0) return x-y;
-    if (strcmp(op, "*") == 0) return x*y;
-    if (strcmp(op, "/") == 0) return x/y;
-    return 0;
-}
-
-long long eval (mpc_ast_t* t) {
+lval eval (mpc_ast_t* t) {
     if ( strstr(t->tag, "number") ) {
-        return atoi(t->contents);
+        // check if there is some error in conversion
+        errno = 0;
+        long long x = strtoll(t->contents, NULL, 10);
+        return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
     }
 
     char* op = t->children[1]->contents;
 
-    long long x = eval(t->children[2]);
+    lval x = eval(t->children[2]);
 
     int i = 3;
     while( strstr(t->children[i]->tag, "expr")  ){
@@ -161,8 +169,8 @@ int main (int argc, char** argv) {
         mpc_result_t r;
         if ( mpc_parse("<stdin>", input, Lispy, &r) ) {
 
-            long long result = eval(r.output);
-            printf("%lld\n", result);
+            lval result = eval(r.output);
+            lval_println(result);
             mpc_ast_delete(r.output);
         }
         else {
